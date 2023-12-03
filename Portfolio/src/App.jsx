8dc71c7b430059388b4b3e71e210545a6e9b01d2
yacob menghistu <yacobabr@gmail.com>
2023-12-03 16:41:48 -0600

    initial

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import AboutPage from './Components/AboutPage'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div><AboutPage /></div>
+    </>
+  )
+}
+
+export default App
---
+++ Portfolio/src/Components/AboutPage.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+export default function AboutPage(){
+
+
+    return(
+        <>
+        <h1>This is the about Page </h1>
+       {/* <Routes>
+            <Route exact path="/contact" element={<ContactPage />} />
+       </Routes> */}
+       </>
+    )
+}
